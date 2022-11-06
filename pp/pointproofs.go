f91eb0b91ff491807b1ab9d9f619f33a6785baee
Yacov Manevich <yacov.manevich@ibm.com>
2022-11-06 12:48:49 +0200

    Implemented point proofs
    Signed-off-by: Yacov Manevich <yacovm@il.ibm.com>

--- pp/pointproofs.go
+++ pp/pointproofs.go
@@ -1,1 +1,107 @@
 package pp
+
+import "C"
+import (
+	"crypto/rand"
+	"fmt"
+	"pol/common"
+
+	math "github.com/IBM/mathlib"
+)
+
+var (
+	c          = math.Curves[1]
+	GroupOrder = c.GroupOrder
+	lambda     = c.FieldBytes
+)
+
+type PP struct {
+	N   int
+	G1s common.G1v
+	G2s common.G2v
+	Gt  *math.Gt
+}
+
+func NewPublicParams(N int) *PP {
+	α := c.NewRandomZr(rand.Reader)
+
+	pp := &PP{N: N}
+
+	g1 := c.GenG1.Copy()
+	g2 := c.GenG2.Copy()
+
+	g1α := func(i int) *math.G1 {
+		return g1.Mul(α.PowMod(c.NewZrFromInt(int64(i))))
+	}
+
+	g2α := func(i int) *math.G2 {
+		return g2.Mul(α.PowMod(c.NewZrFromInt(int64(i))))
+	}
+
+	for i := 1; i <= N; i++ {
+		pp.G1s = append(pp.G1s, g1α(i))
+	}
+
+	// Artificially put the generator instead of g^{a^{N+1}}
+	pp.G1s = append(pp.G1s, c.GenG1.Copy())
+
+	for i := N + 2; i <= 2*N; i++ {
+		pp.G1s = append(pp.G1s, g1α(i))
+	}
+
+	for i := 1; i <= N; i++ {
+		pp.G2s = append(pp.G2s, g2α(i))
+	}
+
+	pp.Gt = c.GenGt.Exp(α.PowMod(c.NewZrFromInt(int64(N + 1))))
+
+	return pp
+}
+
+func Commit(pp *PP, m common.Vec) *math.G1 {
+	if len(m) != pp.N {
+		panic(fmt.Sprintf("message should be of size %d but is of size %d", pp.N, len(m)))
+	}
+
+	var powersOfAlpha common.G1v
+	for i := 0; i < pp.N; i++ {
+		powersOfAlpha = append(powersOfAlpha, pp.G1s[i])
+	}
+
+	return powersOfAlpha.MulV(m).Sum()
+}
+
+func Open(pp *PP, i int, m common.Vec) (mi *math.Zr, π *math.G1) {
+	if i >= pp.N {
+		panic(fmt.Sprintf("can only open an index in [0,%d]", pp.N-1))
+	}
+
+	shift := pp.N - i
+
+	var elements common.G1v
+	var exponents common.Vec
+	for j := 1; j <= pp.N; j++ {
+		if j == i+1 {
+			continue
+		}
+		index := shift + j - 1
+		elements = append(elements, pp.G1s[index])
+		exponents = append(exponents, m[j-1])
+	}
+
+	π = elements.MulV(exponents).Sum()
+	mi = m[i]
+
+	return
+}
+
+func Verify(pp *PP, mi *math.Zr, π *math.G1, C *math.G1, i int) error {
+	left := common.G1v{C}.InnerProd(common.G2v{pp.G2s[pp.N-i-1]})
+	right := common.G1v{π}.InnerProd(common.G2v{c.GenG2})
+	right.Mul(pp.Gt.Exp(mi))
+
+	if left.Equals(right) {
+		return nil
+	}
+	return fmt.Errorf("%v is not an element in index %d in %v", mi, i, C)
+}
