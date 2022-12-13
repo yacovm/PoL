8c5c7d1d3108687b337e6ef2a815479970ef814b
Yacov Manevich <yacov.manevich@ibm.com>
2022-12-13 17:02:18 +0200

    Generalize PoE UT
    Signed-off-by: Yacov Manevich <yacovm@il.ibm.com>

--- poe/poe_test.go
+++ poe/poe_test.go
@@ -1,6 +1,8 @@
 package poe
 
 import (
+	"crypto/rand"
+	"encoding/binary"
 	"pol/common"
 	"pol/pp"
 	"testing"
@@ -10,12 +12,17 @@ import (
 
 func TestProofOfEquality(t *testing.T) {
 
-	publicParams := NewPublicParams(8)
+	n := 64
+	publicParams := NewPublicParams(n)
 
-	v := common.RandVec(8)
-	w := common.RandVec(8)
+	v := common.RandVec(n)
+	w := common.RandVec(n)
 
-	v[1] = w[2]
+	// Select a random index for each vector
+	i := randIndex(t, n)
+	j := randIndex(t, n)
+
+	v[i] = w[j]
 
 	V := pp.Commit(publicParams.PP, v)
 	W := pp.Commit(publicParams.PP, w)
@@ -25,11 +32,19 @@ func TestProofOfEquality(t *testing.T) {
 		PP: publicParams,
 		W:  W,
 		V:  V,
-		I:  1,
-		J:  2,
+		I:  i,
+		J:  j,
 	}
 
 	proof := eq.Prove(v, w)
 	err := eq.Verify(proof)
 	assert.NoError(t, err)
+}
+
+func randIndex(t *testing.T, n int) int {
+	buff := make([]byte, 16)
+	_, err := rand.Read(buff)
+	assert.NoError(t, err)
+
+	return int(binary.BigEndian.Uint16(buff) % uint16(n))
 }
